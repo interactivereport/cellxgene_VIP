2045eaab2fe95f805fb45801d50ce2908887d783
Baohong Zhang <bzhang1@omicsoft01.hpc.biogen.com>
2020-05-11 15:56:56 -0400

    Added VIP to titles

--- update.v1.0.3.sh
+++ update.v1.0.3.sh
@@ -8,10 +8,15 @@ echo $strPath
 sed -i "s|biogen|VIP|g" "$strPath/server/app/app.py"
 
 ## update the cellxgene title to cellxgene VIP
-sed -i "s|gene|geneVIP|g" "cellxgene/client/index_template.html"
-sed -i "s|gene|geneVIP|g" "cellxgene/client/index.html"
-sed -i "s|PLOTTING PANEL|Visualization in Plugin|g" "cellxgene/client/index_template.html"
-cd cellxgene/client; make build; cd ..;make install-dist;cd ..;
+sed -i "s|cell&times;gene|cellxgene VIP|" "cellxgene/client/index_template.html"
+sed -i "s|title=\"cellxgene|title=\"cellxgene VIP|" "cellxgene/client/src/components/app.js"
+sed -i "s|  gene|  gene VIP|" "cellxgene/client/src/components/leftSidebar/topLeftLogoAndTitle.js"
+
+sed -i "s|PLOTTING PANEL|Visualization in Plugin|" "cellxgene/client/index_template.html"
+
+cd cellxgene/client; make build; cp build/index.html $strPath/server/common/web/templates/
+
+cd ../..
 
 cp VIPInterface.py $strPath/server/app/.
 cp interface.html $strPath/server/common/web/static/.
