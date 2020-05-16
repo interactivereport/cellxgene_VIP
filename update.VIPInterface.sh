178a4b2df24cc473f4bdf70f4b502a54fa9f8609
Baohong Zhang <bzhang1@omicsoft01.hpc.biogen.com>
2020-05-16 08:09:05 -0400

    Added splitter, Beautified UI

--- update.VIPInterface.sh
+++ update.VIPInterface.sh
@@ -3,5 +3,6 @@ strPath="$(python -c 'import site; print(site.getsitepackages())')"
 strPath=${strPath//"['"/}
 strPath=${strPath//"']"/}
 echo $strPath
+
 cp VIPInterface.py $strPath/server/app/.
 cp interface.html $strPath/server/common/web/static/.
