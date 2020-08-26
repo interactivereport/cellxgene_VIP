b500aed0a91c508366e8d3af8759687e28176db5
baohongz <baohongz@gmail.com>
2020-08-26 07:23:35 -0400

    Clean gh-pages branch

--- stackedbar/endAll.js
+++
@@ -1,7 +0,0 @@
-function endAll(transition, callback) {
-	var n = 0;
-	transition
-		.each(function() { ++n; })
-		.each("end.endAll", function() { if (!--n) callback.apply(this, arguments); });
-	if(transition.empty()) callback();
-}
