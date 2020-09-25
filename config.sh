d036672568ba3e678e37c9abf8ea63279ae1322b
z5ouyang <z5ouyang@ucsd.edu>
2020-09-24 23:47:38 -0500

    added jq installation into config

--- config.sh
+++ config.sh
@@ -9,7 +9,7 @@ fi
 
 ## buld the cellxgene and install -----------
 #conda remove -y PyYAML
-conda install -c conda-forge -y nodejs=13 fsspec=0.8.2
+conda install -c conda-forge -y nodejs=13 fsspec=0.8.2 jq=1.6
 
 ## obtain a clean version cellxgene a specific version by sha key
 rm -rf cellxgene
