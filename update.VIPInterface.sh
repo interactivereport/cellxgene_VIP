b4082470712fc605b850758f9c2011e324a000f3
z5ouyang <z5ouyang@ucsd.edu>
2020-10-21 00:40:45 -0500

    update

--- update.VIPInterface.sh
+++ update.VIPInterface.sh
@@ -8,12 +8,12 @@ strPath=$(python -c "import server as _; print(_.__file__.replace('/server/__ini
 strweb="${strPath}/server/common/web/static/."
 
 cp VIPInterface.py $strPath/server/app/.
-cp bubbleMap.R $strPath/server/app/.
 cp interface.html $strweb
 
 if [ -n "$1" ]; then
 cp -R stackedbar $strweb
 cp -R d3plot $strweb
+cp bubbleMap.R $strPath/server/app/.
 find ./cellxgene/server/ -name "decode_fbs.py" -exec cp {} $strPath/server/app/. \;
 fi
 
