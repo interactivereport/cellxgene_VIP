bdcf0277845278d9105f452e1cb05a19e5455ac2
z5ouyang <z5ouyang@ucsd.edu>
2021-10-30 23:31:36 -0500

    fix macOS update

--- update.VIPInterface.sh
+++ update.VIPInterface.sh
@@ -16,14 +16,20 @@ mkdir -p $strPath/server/app/gsea
 cp gsea/*gmt $strPath/server/app/gsea
 
 if [ -n "$1" ]; then
-cp Density2D.R $strPath/server/app/.
-cp bubbleMap.R $strPath/server/app/.
-cp violin.R $strPath/server/app/.
-cp volcano.R $strPath/server/app/.
-cp browserPlot.R $strPath/server/app/.
-sed -i "s|route(request.data,current_app.app_config, \"/tmp\")|route(request.data,current_app.app_config)|" "$strPath/server/app/app.py"
-sed -i "s|MAX_LAYOUTS *= *[0-9]\+|MAX_LAYOUTS = 300|" "$strPath/server/common/constants.py"
-find ./cellxgene/server/ -name "decode_fbs.py" -exec cp {} $strPath/server/app/. \;
+  cp Density2D.R $strPath/server/app/.
+  cp bubbleMap.R $strPath/server/app/.
+  cp violin.R $strPath/server/app/.
+  cp volcano.R $strPath/server/app/.
+  cp browserPlot.R $strPath/server/app/.
+  if [[ "$(uname -s)"="Darwin" ]]; then
+    sed -i .bak "s|route(request.data,current_app.app_config, \"/tmp\")|route(request.data,current_app.app_config)|" "$strPath/server/app/app.py"
+    sed -i .bak "s|MAX_LAYOUTS *= *[0-9]\+|MAX_LAYOUTS = 300|" "$strPath/server/common/constants.py"
+  else
+    sed -i "s|route(request.data,current_app.app_config, \"/tmp\")|route(request.data,current_app.app_config)|" "$strPath/server/app/app.py"
+    sed -i "s|MAX_LAYOUTS *= *[0-9]\+|MAX_LAYOUTS = 300|" "$strPath/server/common/constants.py"
+  fi
+
+  find ./cellxgene/server/ -name "decode_fbs.py" -exec cp {} $strPath/server/app/. \;
 fi
 
 echo -e "\nls -l $strweb\n"
