8c45d617de2b0f73d3b59cb697748064695ff424
EdwardAgboraw <edward.agboraw@gmail.com>
2022-04-04 15:48:22 +0100

    Added basic DE Table functionality to the Cell Pop View Tab

--- VIPInterface.py
+++ VIPInterface.py
@@ -305,7 +305,8 @@ def distributeTask(aTask):
     'saveTest':saveTest,
     'getBWinfo':getBWinfo,
     'plotBW':plotBW,
-    'CPV':CellPopView
+    'CPV':CellPopView,
+    'CPVTable':cpvTable
   }.get(aTask,errorTask)
 
 def HELLO(data):
@@ -1568,3 +1569,43 @@ def CellPopView(data):
     CellPopPlot = plt.gcf()
     
     return iostreamFig(CellPopPlot)
+
+def cpvTable(data):
+
+  adata = createData(data)
+
+  #subset by cluster
+  Cluster_Key = data['ClusterKey']
+    
+  Cluster = data['Cluster']
+    
+  adata = adata[adata.obs[Cluster_Key].isin([Cluster])]
+
+  #remove extraneous conditions
+  
+  Condition_Key = data['ConditionKey']
+    
+  Condition1 = data['Cond1']
+    
+  Condition2 = data['Cond2']
+    
+  adata = adata[adata.obs[Condition_Key].isin([Condition1,Condition2])]
+
+  #Differential Expression Analysis
+  res = de.test.t_test(adata,grouping=Condition_Key)
+
+  deg = res.summary()
+  deg = deg.sort_values(by=['qval']).loc[:,['gene','log2fc','pval','qval']]
+  deg['log2fc'] = -1 * deg['log2fc']
+
+  gInfo = getVar(data)
+  deg.index = deg['gene']
+  deg = pd.concat([deg,gInfo],axis=1,sort=False)
+
+  restable = deg.to_html()
+  
+  ppr.pprint("Basic HTML Table created")
+
+  return json.dumps(restable)
+
+
