a408b0d41d004e2b189c26ae9ac91184f02ab269
baohongz <baohongz@gmail.com>
2022-04-11 08:53:28 -0400

    docs/web-source.html -> docs/web-resource.html

---
+++ tutorial/makepdf.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+
+
+# vi cellxgene_VIP.tex
+# :g/includegraphics{https:\/\/interactivereport.github.io\/cellxgene_VIP\/tutorial\//s//includegraphics{/g
+
+# Google and download missing latex style files into this directory structure
+#    /Users/bzhang1/Library/texmf/tex/latex
+#    US-M095355:latex bzhang1$ tree
+#    .
+#    ├── authblk
+#    │   └── authblk.sty
+#    ├── chemgreek
+#    │   └── chemgreek.sty
+#    ├── framed
+#    │   └── framed.sty
+#    ├── fvextra
+#    │   └── fvextra.sty
+#    ├── mhchem
+#    │   └── mhchem.sty
+#    ├── multirow
+#    │   └── multirow.sty
+#    ├── subfigure
+#    │   └── subfigure.sty
+#    ├── threeparttable
+#    │   └── threeparttable.sty
+#    └── todonotes
+#        └── todonotes.sty
+
+
+# Then run
+# pdflatex -jobname=cellxgene_VIP cellxgene_VIP.tex 
