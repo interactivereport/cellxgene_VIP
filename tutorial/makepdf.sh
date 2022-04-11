2053a1cbe0ed15d4c37f1353c7ec9c5f3c28a39c
baohongz <baohongz@gmail.com>
2022-04-11 09:06:08 -0400

    Small change

--- tutorial/makepdf.sh
+++ tutorial/makepdf.sh
@@ -5,11 +5,31 @@ bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 # vi cellxgene_VIP.tex
 # :g/includegraphics{https:\/\/interactivereport.github.io\/cellxgene_VIP\/tutorial\//s//includegraphics{/g
 
-# Google and download missing latex style files into this directory structure
+# Google and download missing latex style files to the same directory as .tex file or into this directory structure
 #    /Users/bzhang1/Library/texmf/tex/latex
 #    US-M095355:latex bzhang1$ tree
-
-
+#    .
+#    ├── algorithmic
+#    │   └── algorithmic.sty
+#    ├── authblk
+#    │   └── authblk.sty
+#    ├── chemgreek
+#    │   └── chemgreek.sty
+#    ├── framed
+#    │   └── framed.sty
+#    ├── fvextra
+#    │   └── fvextra.sty
+#    ├── mhchem
+#    │   └── mhchem.sty
+#    ├── multirow
+#    │   └── multirow.sty
+#    ├── subfigure
+#    │   └── subfigure.sty
+#    ├── threeparttable
+#    │   └── threeparttable.sty
+#    └── todonotes
+#        └── todonotes.sty
+#    
 
 
 # Then run
