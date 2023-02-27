8545a336e955e474750f9fd130a2ec5011ed7fcb
Ed <edward.agboraw@gmail.com>
2023-02-27 15:42:32 +0000

    half-functional back-end

--- VIPInterface.py
+++ VIPInterface.py
@@ -2130,22 +2130,80 @@ def get_HostParasiteTable(data):
   return json.dumps(res)
 
 def hp_ClusterMarkers(data):
+
+  ppr.pprint("Starting functions")
+
+  annot = data['annot']
+  prefix = data['host-parasite']
   
   with app.get_data_adaptor() as data_adaptor:
     copyData = data_adaptor.data.copy()
 
   keepGenes = []
-  prefix = data['host-parasite']
+  
+
+  copyData.var_names = copyData.var["Host-Parasite"].values
 
   for x in copyData.var_names:
     if x.startswith(prefix):
         keepGenes.append(x)
 
+  ppr.pprint("created keepGenes")
+
   # Split Data ----------
 
   keep = copyData[:,keepGenes]
 
+  ppr.pprint("Subset Data, starting CM analysis")
+
   # Run Cluster Marker Analysis
 
+  #nval = int(data["n_value"])
+
+  #de_method = data["DEmethod"]
+
+  # Generate Cluster Markers
+
+  sc.tl.rank_genes_groups(copyData, annot, use_raw=False)
+
+  result = copyData.uns['rank_genes_groups']
+  groups = result['names'].dtype.names
+
+  # Extract top markers for each Cluster.
+
+  genes = []
+  for x in groups: # Get top marker genes for each Cluster.
+    y = pd.DataFrame(copyData.uns['rank_genes_groups']['names'][x]).head(5).values
+    for gene in y:
+      genes.append(gene[0])
+
+  pvals = []
+  for x in groups: # Get p-value of each marker gene.
+    y = pd.DataFrame(copyData.uns['rank_genes_groups']['pvals_adj'][x]).head(5).values
+    for pval in y:
+      val = float(pval[0])
+      final_val = round(val,5)
+      pvals.append(final_val)
+
+  lfcs = []
+  for x in groups: # Get log-fold-change of each marker gene.
+    y = pd.DataFrame(copyData.uns['rank_genes_groups']['logfoldchanges'][x]).head(5).values
+    for lfc in y:
+      val = float(lfc[0])
+      final_val = round(val,2)
+      lfcs.append(final_val)
+
+  clusters = []
+  for x in groups: # Create Cluster Label column
+    for i in range(5):
+      clusters.append(x)
+
+  d = {'Cluster':clusters,'Genes':genes,"LogFoldChange":lfcs,"pvalue_adjusted":pvals}
+
+  df = pd.DataFrame(data=d)
+
+  res = df.to_csv(index=False)
+
+  return json.dumps(res)
   
   
