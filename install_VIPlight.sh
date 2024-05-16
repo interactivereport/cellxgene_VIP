5b69c0fd35d2816606b5917be54c943df4993c0e
z5ouyang <z5ouyang@ucsd.edu>
2024-05-16 16:41:02 -0500

    improve VIPlight installation

--- install_VIPlight.sh
+++ install_VIPlight.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 # CLI will not be available for this installation
-# Please provide the destnation conda env path (appPATH) below
+# Please provide the destnation conda env path (appEnvPath) below
 # if SSL certificate (../...crt) needs to be added into this conda env,
 # please export environment variabble "CONDA_SSL" with the path to the certificate file
-appPATH="~/.conda/envs/VIP"
+appEnvPath="${1:-~/.conda/envs/VIP}"
+appEnvPath=$(realpath ${appEnvPath/\~/$HOME})
+
+echo -e "\nInstallation location (first position parameter): $appEnvPath"
+echo -e "Warning: The above env will be removed if it exists!"
+echo "Wait for 10 seconds, use Ctrl+C to terminate"
+for i in {10..1}; do echo -ne "$i\033[0K\r"; sleep 1; done;
 
 set -e
 exePath=$(readlink -e $(dirname $0))
@@ -21,13 +27,18 @@ condaPath=$(dirname $(dirname $condaPath))
 source $condaPath/etc/profile.d/conda.sh
 
 ## create conda env
-conda env remove -p $appPATH
-conda create -y python=3.8.15 mamba=0.15.3 git=2.39.1 jq=1.6 nodejs=18.12.1 -c conda-forge -p $appPATH #nodejs=13.13.0
+conda env remove -p $appEnvPath
+conda create -y python=3.8.15 mamba=0.15.3 git=2.39.1 jq=1.6 nodejs=18.12.1 -c conda-forge -p $appEnvPath #nodejs=13.13.0
+
+sed "s|CONDA_PATH|$appEnvPath|g" env_yml/VIPlight.yml > env_yml/VIPlight_local.yml
 if [[ -n "$CONDA_SSL" ]] &&  [[ -f "$CONDA_SSL" ]]; then
-    cat $CONDA_SSL >> $appPATH/ssl/cacert.pem
+    cat $CONDA_SSL >> $appEnvPath/ssl/cacert.pem
+    echo -e "  GIT_SSL_CAPATH: $CONDA_SSL" >> VIPlight_local.yml
+    echo -e "  REQUESTS_CA_BUNDLE: $CONDA_SSL" >> VIPlight_local.yml
+    echo -e "  SSL_CERT_FILE: $CONDA_SSL" >> VIPlight_local.yml
 fi
-source $condaPath/etc/profile.d/conda.sh
-conda activate $appPATH
+source $appEnvPath/etc/profile.d/conda.sh
+conda activate
 which python
 
 pip install --force-reinstall flask==2.2.3 flask-cors==3.0.10 flask-restful==0.3.9 flask-talisman==1.0.0 werkzeug==2.2.3 anndata==0.8.0 h5py==3.8.0 pandas==1.5.3 numpy==1.22.0
@@ -62,16 +73,19 @@ cd ..
 
 # install the rest of packages
 which mamba
-mamba env update -f $exePath/env_yml/VIPlight.yml
+mamba env update -f $exePath/env_yml/VIPlight_local.yml
 # update the VIP
 $exePath/update.VIPInterface.sh all
 
+# setup the env
+echo "export VIPenv='source $appEnvPath/etc/profile.d/conda.sh;conda activate'" > $exePath/bin/.env
+
 # setup the update
-echo -e "#"'!'"/usr/bin/env bash\nsource $condaPath/etc/profile.d/conda.sh\nconda activate $appPATH\ncd $exePath\ngit pull\n./update.VIPInterface.sh all\n" > $exePath/update
+echo -e "#"'!'"/usr/bin/env bash\nsource $exePath/bin/.env\neval \$VIPenv\ncd $exePath\ngit pull\n./update.VIPInterface.sh all\n" > $exePath/update
 chmod a+x "$exePath/update"
 
 # setup the VIPlight
-echo -e "#"'!'"/usr/bin/env bash\nexport PYTHONNOUSERSITE=1\nsource $condaPath/etc/profile.d/conda.sh\nconda activate $appPATH\ncellxgene \"\$@\"" > $exePath/VIPlight
+echo -e "#"'!'"/usr/bin/env bash\nsource $exePath/bin/.env\neval \$VIPenv\ncellxgene \"\$@\"" > $exePath/VIPlight
 chmod a+x "$exePath/VIPlight"
 
 echo
